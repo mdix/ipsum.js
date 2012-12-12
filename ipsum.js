210be6df7d1d6a1605de4e7a677f0a27977ca1dc
mdix <dix@mediaman.de>
2012-12-12 23:02:09 +0100

    inital commit

---
+++ ipsum.js
@@ -0,0 +1,40 @@
+var ipsumJs = {};
+ipsumJs.config = {};
+ipsumJs.config.htmlTags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'td', 'span'];
+
+ipsumJs.init = function() {
+    console.log('ipsum.js loaded, use alt+ or alt- to increase / decrease text quantity in ' + ipsumJs.config.htmlTags + ' tags');
+    ipsumJs.keybindings();
+}
+
+ipsumJs.keybindings = function() {
+    document.onkeydown = function(key) {
+        if (key.altKey && key.keyIdentifier === "U+00BB") {
+            ipsumJs.moreText();
+        }
+        if (key.altKey && key.keyIdentifier === "U+00BD") {
+            ipsumJs.lessText();
+        }
+    }
+}
+
+ipsumJs.moreText = function() {
+    for (var i = 0; i < ipsumJs.config.htmlTags.length; i++) {
+        var elems = document.getElementsByTagName(ipsumJs.config.htmlTags[i]);
+        for (var ii=0; ii < elems.length; ii++) {
+            elems[ii].innerText = elems[ii].innerText + elems[ii].innerText;
+        }
+    }
+}
+
+ipsumJs.lessText = function() {
+    for (var i = 0; i < ipsumJs.config.htmlTags.length; i++) {
+        var elems = document.getElementsByTagName(ipsumJs.config.htmlTags[i]);
+        for (var ii=0; ii < elems.length; ii++) {
+            var textLength = elems[ii].innerText.length;
+            elems[ii].innerText = elems[ii].innerText.substring(textLength*0.1, textLength);
+        }
+    }
+}
+
+ipsumJs.init();
